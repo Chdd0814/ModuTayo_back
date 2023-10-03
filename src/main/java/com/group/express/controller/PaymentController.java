710b458ac03888c73c5e9c80f1a1a04db3be42e9
JunBeom Hwang <wnsqja4646@gmail.com>
2023-10-03 23:27:27 +0900

    결제 CRUD 개발중

--- src/main/java/com/group/express/controller/PaymentController.java
+++ src/main/java/com/group/express/controller/PaymentController.java
@@ -2,17 +2,16 @@ package com.group.express.controller;
 
 import com.group.express.domain.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.group.express.service.PaymentService;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/payment")
 public class PaymentController {
 
     private final PaymentService PaymentService;
@@ -24,4 +23,17 @@ public class PaymentController {
     public ResponseEntity<List<Payment>> getBusBooking(@PathVariable String id) {
         return ResponseEntity.ok(PaymentService.getPaymentList(id));}
 
-}
+    @PostMapping("/getPaymentinfo")
+    public ResponseEntity<String> handlePaymentSuccess(@RequestBody Payment payment) {
+        try {
+            PaymentService.PaymentSuccess(payment);
+            return ResponseEntity.ok("Payment success");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Payment failed");
+        }
+    }
+    }
+
+
+
+
